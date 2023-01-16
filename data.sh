1d675c2da661f2f17edad76a0195cb7eeeb9ca5b
Tomiwa Ademidun <tomiademidun@gmail.com>
2023-01-16 07:34:11 -0500

    add script to save data locally

--- data.sh
+++ data.sh
@@ -10,3 +10,13 @@ load_remote_data(){
       --app atila-core-service > dumpdata.json;
  fi
 }
+
+load_local_data(){
+ # example: load_local_data atlas.document
+ if [ -z "$2" ]
+   then
+     python manage.py dumpdata $1 --indent 4 --natural-primary --natural-foreign > dumpdata.json ;
+ else
+     python manage.py dumpdata $1 --pks $2 --indent 4 --natural-primary --natural-foreign > dumpdata.json;
+ fi
+}
