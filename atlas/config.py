20248c915fe80d91ca7b2d64369ceacb02e62652
Tomiwa Ademidun <tomiademidun@gmail.com>
2023-01-16 00:51:16 -0500

    put huggingface endpoint url into the code
    it's not necessarily a secret since it's protected by the API key

--- atlas/config.py
+++ atlas/config.py
@@ -1,6 +1,6 @@
 import os
 
-HUGGING_FACE_ENDPOINT_URL = os.environ.get('HUGGING_FACE_URL', None)
+HUGGING_FACE_ENDPOINT_URL = "https://rl2hxotyspedkt19.us-east-1.aws.endpoints.huggingface.cloud"
 
 HUGGING_FACE_API_KEY = os.environ.get('HUGGING_FACE_API_KEY', None)
 
