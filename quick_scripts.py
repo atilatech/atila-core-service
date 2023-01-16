9700173e58e5a32ba03db9666845ce230ab94d3c
Tomiwa Ademidun <tomiademidun@gmail.com>
2023-01-15 22:31:09 -0500

    added transcription example: 'Making Friends with Machine Learning: Regression'

--- quick_scripts.py
+++ quick_scripts.py
@@ -14,12 +14,13 @@ django.setup()
 # import statements must come AFTER django.setup() has been called
 from atlas.transcribe import get_transcript_from_youtube
 
-url = "https://www.youtube.com/watch?v=aUlLLnsvwKE"  # 24 Hours With Emma Chamberlain in Copenhagen | Vogue
+# url = "https://www.youtube.com/watch?v=aUlLLnsvwKE"  # 24 Hours With Emma Chamberlain in Copenhagen | Vogue
+url = "https://www.youtube.com/watch?v=WNvOtwP_yf4"  # Making Friends with Machine Learning: Regression
 # Sam Parr: From Jail To $20+ Million By 32 | The Danny Miranda Podcast 243
 # url = "https://www.youtube.com/watch?v=xKSIOI-wpSQ"
 # INSANE GAME! Los Angeles Lakers vs Dallas Mavericks Final Minutes ! 2022-23 NBA Season
 # url = "https://www.youtube.com/watch?v=31DsvAWXryY"
 
-# print("get_manual_transcript(url)", get_manual_transcript(url))
+# print("get_transcript_from_youtube(url)", get_transcript_from_youtube(url))
 
-print("get_manual_transcript(url)", get_transcript_from_youtube(url))
+print("get_transcript_from_youtube(url)", get_transcript_from_youtube(url, 'json'))
